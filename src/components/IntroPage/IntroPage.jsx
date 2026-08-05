bc81e30d8becd29ba697b4224d48ba4375e5c83b
GHL <gustavhl@hotmail.com>
2026-08-05 17:46:43 +0200

    Readded SVG lines will potentially animate with hover effect later.

--- src/components/IntroPage/IntroPage.jsx
+++ src/components/IntroPage/IntroPage.jsx
@@ -1,9 +1,11 @@
 import './IntroPage.css'
+import Lines from "../../components/Lines.jsx"
 
 const IntroPage = () => {
     return (
         <>
             <section className="relative min-h-screen w-full flex items-center justify-center overflow-hidden">
+                <Lines />
                 <div className="absolute top-1/2 -translate-y-1/2 -left-10 sm:-left-14   z-0 pointer-events-none select-none flex flex-col items-center gap-1 sm:gap-3 font-serif text-[26vh] sm:text-[32vh] font-bold text-white tracking-tighter leading-[1]">
                     <span className="rotate-[15deg]">G</span>
                     <span className="rotate-[15deg] -translate-x-4 sm:-translate-x-8">H</span>
---
+++ src/components/Lines.jsx
@@ -0,0 +1,21 @@
+const Lines = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      className="inset-0 h-screen w-screen pointer-events-none z-10"
+      preserveAspectRatio="none"
+      viewBox="0 0 100 100"
+    >
+      <g
+        stroke="#ffffff"
+        strokeWidth="0.2"
+      >
+        <line x1="35%" y1="20%" x2="100%" y2="20%" />
+        <line x1="55%" y1="25%" x2="100%" y2="25%" />
+        <line x1="70%" y1="30%" x2="100%" y2="30%" />
+      </g>
+    </svg>
+  );
+}
+
+export default Lines
